50f5beb066014c87b03bd8c878068cfd9c096fbe
Javier Romero <javier.romero@bottlerocketapps.com>
2016-06-01 16:37:42 -0500

    Some initial styling...

--- src/main/resources/js/bulletin.js
+++ src/main/resources/js/bulletin.js
@@ -1,14 +1,39 @@
-AJS.$.get("/jira/rest/auth/1/session", function (data) {
-    var userName = data.name;
-    alert("hello " + userName);
+//AJS.$.get("/jira/rest/bulletin/1/boards/TP1/bulletins.json", function (data) {
 //
-//    // check groups
-//    AJS.$.get("/jira/rest/api/2.0.alpha1/user", {username: userName, expand: "groups"}, function (data) {
-//        var groups = data.groups;
-//        var groupItems = jQuery.map(groups.items, function (val, j) {return val.name});
-//
-//        if (jQuery.inArray("jira-administrators", groups.items)) {
-//            ...
-//        }
-//    });
+//});
+
+AJS.toInit(function(){
+    var html = ' \
+    <div id="bulletins-container"> \
+      <a id="bulletins-tab-label" class="aui-button aui-button-primary aui-style">Bulletin&nbsp;Boards</a> \
+      <div class="aui-page-panel"> \
+        <div class="aui-page-panel-inner"> \
+            <section class="aui-page-panel-content"> \
+                <h2>Bulletin Boards</h2> \
+                <div class="aui-tabs horizontal-tabs"> \
+                    <ul class="tabs-menu"> \
+                        <li class="menu-item active-tab"> \
+                            <a href="#tabs-example-first">Tab 1</a> \
+                        </li> \
+                        <li class="menu-item"> \
+                            <a href="#tabs-example-second">Tab 2</a> \
+                        </li> \
+                    </ul> \
+                    <div class="tabs-pane active-pane" id="tabs-example-first"> \
+                        <h2>This is tab 1</h2> \
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> \
+                    </div> \
+                    <div class="tabs-pane" id="tabs-example-second"> \
+                        <h2>This is tab 2</h2> \
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> \
+                    </div> \
+                </div> \
+            </section> \
+        </div> \
+      </div> \
+    </div> \
+    ';
+    AJS.$("body").append(html);
+
+    AJS.$("#bulletins-tab-label").draggable({ axis: "y", cursor: "move" });
 });
