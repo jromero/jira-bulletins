6d9af6a00075f1ed4ceb721f9da37dcd746f896f
Javier Romero <javier.romero@bottlerocketapps.com>
2016-06-15 23:05:48 -0500

    WIP: UI adjusted

--- src/main/resources/js/bulletin.js
+++ src/main/resources/js/bulletin.js
@@ -1,32 +1,15 @@
-//AJS.$.get("/jira/rest/bulletin/1/boards/TP1/bulletins.json", function (data) {
-//
-//});
-
 AJS.toInit(function(){
+    var panelExpandedWidth = 350;
+
     var html = ' \
     <div id="bulletins-container"> \
       <a id="bulletins-tab-label" class="aui-button aui-button-primary aui-style">Bulletin&nbsp;Boards</a> \
       <div class="aui-page-panel"> \
         <div class="aui-page-panel-inner"> \
-            <section class="aui-page-panel-content"> \
+            <section id="bulletins-content" class="aui-page-panel-content" style="display: none"> \
                 <h2>Bulletin Boards</h2> \
-                <div class="aui-tabs horizontal-tabs"> \
-                    <ul class="tabs-menu"> \
-                        <li class="menu-item active-tab"> \
-                            <a href="#tabs-example-first">Tab 1</a> \
-                        </li> \
-                        <li class="menu-item"> \
-                            <a href="#tabs-example-second">Tab 2</a> \
-                        </li> \
-                    </ul> \
-                    <div class="tabs-pane active-pane" id="tabs-example-first"> \
-                        <h2>This is tab 1</h2> \
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> \
-                    </div> \
-                    <div class="tabs-pane" id="tabs-example-second"> \
-                        <h2>This is tab 2</h2> \
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> \
-                    </div> \
+                <div id="bulletins-tabs" class="aui-tabs horizontal-tabs"> \
+                    <ul id="bulletins-tabs-menu" class="tabs-menu"></ul> \
                 </div> \
             </section> \
         </div> \
@@ -36,4 +19,40 @@ AJS.toInit(function(){
     AJS.$("body").append(html);
 
     AJS.$("#bulletins-tab-label").draggable({ axis: "y", cursor: "move" });
+
+    AJS.$("#bulletins-tab-label").click(function() {
+        var container = AJS.$("#bulletins-container");
+        var content = AJS.$("#bulletins-content");
+
+        if (container.width() == 0) {
+            container.width(panelExpandedWidth);
+            content.show();
+        } else {
+            container.width(0);
+            content.hide();
+        }
+    })
+
+    AJS.$.get(AJS.params.baseURL + "/rest/greenhopper/1.0/xboard/work/allData?rapidViewId=1", function(data) {
+        var projects = data.issuesData.projects
+        if (projects.length > 0) {
+            for (var i = 0; i < projects.length; i++) {
+                AJS.$.get(AJS.params.baseURL + "/rest/api/2/project/" + projects[i].id, function(data) {
+                    AJS.$('#bulletins-tabs-menu').append('<li class="menu-item active-tab"><a>' + data.key + '</a></li>');
+                    AJS.$('#bulletins-tabs').append(
+                        '<div class="tabs-pane active-pane" id="bulletins-tab-pane-' + data.key + '"> \
+                         <a class="aui-button aui-button-primary aui-style">Add</a> \
+                         </div>'
+                    );
+                });
+            }
+        } else {
+            AJS.$("#bulletins-tabs").hide();
+            AJS.messages.generic("#bulletins-content", {
+               title: 'No projects discovered for this board.',
+               body: '<p>As issues are added, we can determine what projects are associated to this board.</p>',
+               closeable: false
+            });
+        }
+    })
 });
