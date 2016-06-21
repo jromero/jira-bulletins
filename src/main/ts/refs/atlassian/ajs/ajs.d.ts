c0d0157447e7496f1362eeea52c140ce8683f2e4
Javier Romero <javier.romero@bottlerocketstudios.com>
2016-06-20 21:10:46 -0500

    Added AJS declarations

--- src/main/ts/refs/atlassian/ajs/ajs.d.ts
+++ src/main/ts/refs/atlassian/ajs/ajs.d.ts
@@ -2,14 +2,61 @@ interface AJSParams {
   baseURL: string
 }
 
+interface AJSDialog2 {
+
+  off(events: string, handler: (eventObject: JQueryEventObject, ...args: any[]) => any): AJSDialog2
+
+  on(event: any, handler: (eventObject: JQueryEventObject, ...args: any[]) => any): AJSDialog2
+
+  show(): AJSDialog2
+
+  hide(): AJSDialog2
+
+  remove(): AJSDialog2
+
+  isVisible(): boolean
+}
+
 interface AJS {
   $: JQueryStatic
   toInit: any
-  log: any
   params: AJSParams
   messages: any
   tabs: any
-  dialog2: any
+
+  /**
+   * Returns the "path" to the application, which is needed when creating absolute urls within the application.
+   *
+   * @since 3.5.5
+   */
+  contextPath(): string
+
+  /**
+   *
+   * @param selector
+   */
+  dialog2(selector: string): AJSDialog2
+
+  /**
+   * Performs html-safe escaping of the input string.
+   *
+   * @since 4.0
+   */
+  escapeHTML(html: string): string
+
+  /**
+   * Provides an easy way to substitute parameters into a string.
+   *
+   * @since 1.0
+   */
+  format(format: string, ...args: any[]): string
+
+  /**
+   * A safe alternative to console.log(). It ensures that both console and the console.log method exist before executing.
+   *
+   * @since 1.0
+   */
+  log(message: string): void
 }
 
 declare var AJS: AJS
